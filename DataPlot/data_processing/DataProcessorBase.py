137b4eaefe4d5c0da246627a77435dc986d3709a
Chih-Yu Chan <alex870521@gmail.com>
2023-12-11 03:09:20 +0800

    create DataProcessorBase.py

--- DataPlot/data_processing/DataProcessorBase.py
+++ DataPlot/data_processing/DataProcessorBase.py
@@ -5,23 +5,14 @@ path_meta = Path(pkg_resources.resource_filename('DataPlot', 'Data-example'))
 
 
 class DataProcessorBase:
-    PATH_MAIN = path_meta
-
-    with open(PATH_MAIN / 'level1' / 'EPB.csv', 'r', encoding='utf-8', errors='ignore') as f:
-        minion = read_csv(f, parse_dates=['Time'], na_values=['-', 'E', 'F']).set_index('Time')
-
-    with open(PATH_MAIN / 'level1' / 'IMPACT.csv', 'r', encoding='utf-8', errors='ignore') as f:
-        impact = read_csv(f, parse_dates=['Time']).set_index('Time')
-
-    with open(PATH_MAIN / 'level2' / 'chemical.csv', 'r', encoding='utf-8', errors='ignore') as f:
-        chemical = read_csv(f, parse_dates=['Time']).set_index('Time')
+    PATH_MAIN = Path(__file__).parents[2] / 'Data-example'
 
     def __init__(self, reset=False):
         self.PATH_MAIN = self.PATH_MAIN
         self.reset = reset
-        self.minion = self.minion
-        self.impact = self.impact
-        self.chemical = self.chemical
+        self.minion = None
+        self.impact = None
+        self.chemical = None
         self.improve = None
         self.PSD = None
         self.PESD = None
