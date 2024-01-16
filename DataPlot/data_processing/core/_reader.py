fab4ee5e3ad9972e6360b172c23af285e12088fe
Chih-Yu Chan <alex870521@gmail.com>
2024-01-16 11:42:19 +0800

    update _reader.py

--- DataPlot/data_processing/core/_reader.py
+++ DataPlot/data_processing/core/_reader.py
@@ -32,20 +32,26 @@ class DataReader:
 
     DEFAULT_PATH = Path(__file__).parents[2] / 'Data-example'
 
-    def __new__(cls, filename: str) -> Union[DataFrame, None]:
+    def __init__(self, filename):
+        self.file_path: Union[Path, None] = list(self.DEFAULT_PATH.glob('**/' + filename))[0]
+        self.data:      Union[DataFrame, None] = self.read_data(self.file_path)
+
+    def __new__(cls, filename: str) -> 'DataFrame':
+        file_path: Union[Path, None] = cls.find_file(filename)
+        data:      Union[DataFrame, None] = cls.read_data(file_path)
+        return data
+
+    @classmethod
+    def find_file(cls, filename: str) -> Path:
         try:
             file_path = list(cls.DEFAULT_PATH.glob('**/' + filename))[0]
         except IndexError:
             raise FileNotFound(f"File '{filename}' not found.")
         else:
-            return cls.read_data(file_path)
-
-    def __init__(self, filename):
-        self.file_path = list(self.DEFAULT_PATH.glob('**/' + filename))[0]
-        self.data: DataFrame = self.read_data(self.file_path)
+            return file_path
 
     @classmethod
-    def read_data(cls, file_path) -> DataFrame:
+    def read_data(cls, file_path) -> Union[DataFrame, None]:
         file_extension = file_path.suffix.lower()
 
         if file_extension == '.csv':
@@ -58,15 +64,13 @@ class DataReader:
             raise ValueError(f"Unsupported file format: {file_extension}")
 
     @staticmethod
-    def read_csv(file_path):
-        with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-            return read_csv(f, parse_dates=['Time'], na_values=['-', 'E', 'F'], low_memory=False).set_index('Time')
+    def read_csv(file_path) -> DataFrame:
+        return read_csv(file_path, parse_dates=['Time'], na_values=['-', 'E', 'F'], low_memory=False).set_index('Time')
 
     @staticmethod
-    def read_json(file_path):
-        with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-            return read_json(f)
+    def read_json(file_path) -> DataFrame:
+        return read_json(file_path)
 
     @staticmethod
-    def read_excel(file_path):
+    def read_excel(file_path) -> DataFrame:
         return read_excel(file_path, parse_dates=['Time'])
