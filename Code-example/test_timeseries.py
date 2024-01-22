0ea775ff88d13c780b8132cdcc07b6ed4423a633
Chih-Yu Chan <alex870521@gmail.com>
2024-01-22 15:18:01 +0800

    update

--- Code-example/test_timeseries.py
+++ Code-example/test_timeseries.py
@@ -7,20 +7,19 @@ if __name__ == '__main__':
     PSSD = DataReader('PSSD_dSdlogdp.csv')
     PVSD = DataReader('PVSD_dVdlogdp.csv')
     PESD = DataReader('PESD_dextdlogdp_internal.csv')
-    df   = DataReader('All_data.csv')
+    Data = DataReader('All_data.csv')
 
     # Season timeseries
     for season, (st_tm_, fn_tm_) in Seasons.items():
         st_tm, fn_tm = pd.Timestamp(st_tm_), pd.Timestamp(fn_tm_)
 
-        df = df.loc[st_tm:fn_tm].copy()
+        df = Data.loc[st_tm:fn_tm].copy()
 
         PNSD_data = PNSD.loc[st_tm:fn_tm]
         PSSD_data = PSSD.loc[st_tm:fn_tm]
 
         # 數據平滑
-        df = df.rolling(3).mean(numeric_only=True)
+        # df = df.rolling(3).mean(numeric_only=True)
 
         time_series(df)
 
-        break
