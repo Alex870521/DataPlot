4bab60c12a0bbacf7ac754be9390b420d372e0c1
Chih-Yu Chan <alex870521@gmail.com>
2024-07-26 13:46:52 +0800

    remove tqdm and add the autolayout parameter

--- DataPlot/plot/core/_decorator.py
+++ DataPlot/plot/core/_decorator.py
@@ -1,7 +1,6 @@
 from functools import wraps
 
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 
 __all__ = ['set_figure']
 
@@ -14,51 +13,51 @@ def set_figure(func=None,
                figsize: tuple | None = None,
                fs: int | None = None,
                fw: str = None,
+               autolayout: bool = True
                ):
     def decorator(_func):
         @wraps(_func)
         def wrapper(*args, **kwargs):
-            with tqdm(total=1, desc=f"Plot: {_func.__name__}", bar_format="{l_bar}{bar}|") as progress:
-                plt.rcParams['mathtext.fontset'] = 'custom'
-                plt.rcParams['mathtext.rm'] = 'Times New Roman'
-                plt.rcParams['mathtext.it'] = 'Times New Roman: italic'
-                plt.rcParams['mathtext.bf'] = 'Times New Roman: bold'
-                plt.rcParams['mathtext.default'] = 'regular'
+            print(f'\t\t\t Plot: \033[96m{_func.__name__}\033[0m')
 
-                # The font properties used by `text.Text`.
-                # The text, annotate, label, title, ticks, are used to create text
-                plt.rcParams['font.family'] = 'Times New Roman'
-                plt.rcParams['font.weight'] = fw or 'normal'
-                plt.rcParams['font.size'] = fs or 12
+            plt.rcParams['mathtext.fontset'] = 'custom'
+            plt.rcParams['mathtext.rm'] = 'Times New Roman'
+            plt.rcParams['mathtext.it'] = 'Times New Roman: italic'
+            plt.rcParams['mathtext.bf'] = 'Times New Roman: bold'
+            plt.rcParams['mathtext.default'] = 'regular'
 
-                plt.rcParams['axes.titlesize'] = 'large'
-                plt.rcParams['axes.titleweight'] = 'bold'
-                plt.rcParams['axes.labelweight'] = 'bold'
+            # The font properties used by `text.Text`.
+            # The text, annotate, label, title, ticks, are used to create text
+            plt.rcParams['font.family'] = 'Times New Roman'
+            plt.rcParams['font.weight'] = fw or 'normal'
+            plt.rcParams['font.size'] = fs or 12
 
-                plt.rcParams['xtick.labelsize'] = 'medium'
-                plt.rcParams['ytick.labelsize'] = 'medium'
+            plt.rcParams['axes.titlesize'] = 'large'
+            plt.rcParams['axes.titleweight'] = 'bold'
+            plt.rcParams['axes.labelweight'] = 'bold'
 
-                # matplotlib.font_manager.FontProperties ---> matplotlib.rcParams
-                plt.rcParams['legend.loc'] = 'best'
-                plt.rcParams['legend.frameon'] = False
-                plt.rcParams['legend.fontsize'] = 'small'
-                plt.rcParams['legend.title_fontsize'] = 'medium'
-                plt.rcParams['legend.handlelength'] = 1.5
+            plt.rcParams['xtick.labelsize'] = 'medium'
+            plt.rcParams['ytick.labelsize'] = 'medium'
 
-                plt.rcParams['figure.figsize'] = figsize or (5, 4)
-                plt.rcParams['figure.dpi'] = 200
-                plt.rcParams['figure.subplot.left'] = 0.125
-                plt.rcParams['figure.subplot.right'] = 0.875
-                plt.rcParams['figure.subplot.bottom'] = 0.125
-                plt.rcParams['figure.subplot.top'] = 0.88
-                # plt.rcParams['figure.autolayout'] = True
-                # plt.rcParams['figure.constrained_layout.use'] = True
+            # matplotlib.font_manager.FontProperties ---> matplotlib.rcParams
+            plt.rcParams['legend.loc'] = 'best'
+            plt.rcParams['legend.frameon'] = False
+            plt.rcParams['legend.fontsize'] = 'small'
+            plt.rcParams['legend.title_fontsize'] = 'medium'
+            plt.rcParams['legend.handlelength'] = 1.5
 
-                plt.rcParams['savefig.transparent'] = True
+            plt.rcParams['figure.figsize'] = figsize or (5, 4)
+            plt.rcParams['figure.dpi'] = 200
+            plt.rcParams['figure.subplot.left'] = 0.125
+            plt.rcParams['figure.subplot.right'] = 0.875
+            plt.rcParams['figure.subplot.bottom'] = 0.125
+            plt.rcParams['figure.subplot.top'] = 0.88
+            plt.rcParams['figure.autolayout'] = autolayout
+            # plt.rcParams['figure.constrained_layout.use'] = True
 
-                result = _func(*args, **kwargs)
+            plt.rcParams['savefig.transparent'] = True
 
-                progress.update()
+            result = _func(*args, **kwargs)
 
             return result
 
