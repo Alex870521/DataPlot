7d56f0ee16678cab4b6bdb918d108bcd0e82330d
Chih-Yu Chan <alex870521@gmail.com>
2024-08-07 01:33:37 +0800

    update

--- DataPlot/plot/templates/__init__.py
+++ DataPlot/plot/templates/__init__.py
@@ -2,6 +2,7 @@ from .contour import *
 from .corr_matrix import corr_matrix
 from .diurnal_pattern import *
 from .koschmieder import *
+from .metal_heatmap import metal_heatmaps, process_data
 from .radar import *
 from .regression import *
 from .scatter import *
